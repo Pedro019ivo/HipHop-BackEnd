3a93f73da979491ed9f6bb12de79cc4d9ce83ee9
Pedro Ivo <pedro019ivo@gmail.com>
2025-10-09 21:03:00 -0300

    inicio da criacao da rota de usuarios

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 //import { clear } from "console"
 import fastify from "fastify"
 import { env } from "./shared/env/environments"
+import { appRoutes } from "./shared/routes/app.routes"
 
 const app = fastify()
 //exemplo de rota get
 // app.get("/hello", () => {
 // return "Bem Vindo"
 // })
+
+app.register(appRoutes)
 
 app
   .listen({
